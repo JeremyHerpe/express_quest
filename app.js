afb7355838877aa0e65f73c4b92e5576529b4a47
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-12 14:42:11 +0200

    quete 4

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+app.listen(port, err => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
@@ -18,19 +26,12 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
-app.listen(port, err => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
-
-app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
